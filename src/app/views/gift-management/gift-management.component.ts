c9756763deef840d4ea98c84ab9ec7fa9a69098c
sahar-letaief <sahar.letaief@esprit.tn>
2023-04-16 12:46:35 +0100

    adding

---
+++ src/app/views/gift-management/gift-management.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { GiftService } from './gift.service';
+import { Router } from '@angular/router';
+import { NgToastService } from 'ng-angular-popup';
+import { Gift } from 'app/Models/gift';
+
+@Component({
+  selector: 'app-gift-management',
+  templateUrl: './gift-management.component.html',
+  styleUrls: ['./gift-management.component.css']
+})
+export class GiftManagementComponent implements OnInit {
+
+  constructor(private gs:GiftService ,router:Router,private toast: NgToastService) { }
+  gifts:Gift[];
+  detailsGift?:Gift;
+  ngOnInit(): void {
+  }
+  getGifts(){
+    this.gs.getAllGifts().subscribe(data => {
+      this.gifts = data;
+    });
+  }
+
+  public OnDetailsGift(idgift: number){
+    this.gs.OnDetailsGift(idgift).subscribe(
+      (response: Gift) => {
+        console.log(response);
+      });
+  }  
+  
+  public onOpenModal(gift: Gift, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+   
+    if(mode == 'details'){
+      this.detailsGift = gift;
+      button.setAttribute('data-target', '#giftDetailsModal');
+    }
+    container?.appendChild(button);
+    button.click();
+  }
+}
---
+++ src/app/views/gift-management/gift.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Gift } from 'app/Models/gift';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GiftService {
+
+  private baseURL = "http://localhost:8082/PharmaLife/Gift";
+
+  constructor(private httpClient: HttpClient) { }
+  
+  
+  getAllGifts(): Observable<Gift[]>{
+    return this.httpClient.get<Gift[]>(`${this.baseURL}`+"/all-gifts");
+  }
+
+  OnDetailsGift(idGift: number): Observable<Gift>{
+    return this.httpClient.get<Gift>(`${this.baseURL}`+"/retrieve-gift/"+`${idGift}`);
+  }
+}
