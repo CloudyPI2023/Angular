2f831cdefb8560d72d3d90c465849423ab9deee8
emna-hannachii <emna.hannachi@esprit.tn>
2023-05-08 21:50:52 +0100

    working on clean version

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,12 +1,6 @@
 import { Routes } from '@angular/router';
-
 import { DashboardComponent } from '../../dashboard/dashboard.component';
-import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
 import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
-import { UpgradeComponent } from '../../upgrade/upgrade.component';
 import { UserComponent } from 'app/views/user-management/user.component';
 import { DeliveryPersonComponent } from 'app/views/deliveryP-management/delivery-person.component';
 import { ProductManagementComponent } from 'app/views/product-management/product-management.component';
@@ -76,12 +70,9 @@ export const AdminLayoutRoutes: Routes = [
     { path: 'category-management', component: CategoryManagementComponent },
     { path: 'gift-management', component: GiftManagementComponent },
     { path: 'reclamation-management', component: ReclamationManagementComponent },
-    { path: 'table-list', component: TableListComponent },
-    { path: 'typography', component: TypographyComponent },
+   
     { path: 'icons', component: IconsComponent },
-    { path: 'maps', component: MapsComponent },
-    { path: 'notifications', component: NotificationsComponent },
-    { path: 'upgrade', component: UpgradeComponent },
+
     { path: 'associations',   component: AssociationComponent },
    
     { path: 'donations',   component: DonationComponent },
